d56e9aae8fa5f96f06d9ce58efc05ad5653508fc
Marcin <teozio@gmail.com>
2014-08-25 14:15:58 -0400

    Private members of Tensor class

--- Tensor.h
+++ Tensor.h
@@ -7,6 +7,23 @@ class Tensor {
 
   private:
 
+    int rank_; // number of dimensions (indices)
+
+    int *dims_; // dimensions of each rank
+
+    int *nBlocks_; // number of blocks for each rank
+
+    int **BlockSize_; // sizes of blocks
+
+    int *BlockOwner_; // id process assigned to each block
+
+    int nTotalBlocks_; // number of total blocks
+    int nLocalBlocks_; // number of local blocks
+
+    int **BlockCoo_; // coordinates of each local block
+
+    double **data_; // data    
+
 };
 
 #endif
